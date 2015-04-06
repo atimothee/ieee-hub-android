34901409b80c3bd7adf20f0072a3be4478b889bf
Timothy Asiimwe <atimothee@gmail.com>
2015-04-06 13:37:15 +0300

    initial commit --project setup

---
+++ app/src/main/java/org/ieee/ieeehub/ConferenceDetailFragment.java
@@ -0,0 +1,62 @@
+package org.ieee.ieeehub;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import org.ieee.ieeehub.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Conference detail screen.
+ * This fragment is either contained in a {@link ConferenceListActivity}
+ * in two-pane mode (on tablets) or a {@link ConferenceDetailActivity}
+ * on handsets.
+ */
+public class ConferenceDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public ConferenceDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_conference_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.conference_detail)).setText(mItem.content);
+        }
+
+        return rootView;
+    }
+}
