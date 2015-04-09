4298cdd42b775d066d1ded836a14a4f89b9be398
Timothy Asiimwe <atimothee@gmail.com>
2015-04-10 01:36:43 +0300

    tweaked nav activity to shoe material tabs

--- app/src/main/java/org/ieee/ieeehub/NavigationDrawerActivity.java
+++ app/src/main/java/org/ieee/ieeehub/NavigationDrawerActivity.java
@@ -2,11 +2,16 @@ package org.ieee.ieeehub;
 
 import android.content.res.Configuration;
 import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,12 +19,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import org.ieee.ieeehub.fragment.ArticleFragment;
-import org.ieee.ieeehub.fragment.BrowseFragment;
 import org.ieee.ieeehub.fragment.ConferenceListFragment;
-import org.ieee.ieeehub.fragment.TabbedFragment;
+
+import java.util.Locale;
 
 
-public class NavigationDrawerActivity extends ActionBarActivity implements ArticleFragment.OnFragmentInteractionListener{
+public class NavigationDrawerActivity extends ActionBarActivity implements ArticleFragment.OnFragmentInteractionListener, ActionBar.TabListener {
 
     private static final String TAG = NavigationDrawerActivity.class.getSimpleName();
 
@@ -30,6 +35,12 @@ public class NavigationDrawerActivity extends ActionBarActivity implements Artic
     private CharSequence mDrawerTitle;
     private CharSequence mTitle;
     private String[] mDrawerItmes;
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +92,40 @@ public class NavigationDrawerActivity extends ActionBarActivity implements Artic
             navigateTo(0);
         }
 
+        // Set up the action bar.
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by
+            // the adapter. Also specify this Activity object, which implements
+            // the TabListener interface, as the callback (listener) for when
+            // this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+
     }
 
     /*
@@ -130,16 +175,16 @@ public class NavigationDrawerActivity extends ActionBarActivity implements Artic
 
         switch(position) {
             case 0:
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.content_frame, new TabbedFragment(), TabbedFragment.TAG).commit();
+//                getSupportFragmentManager()
+//                        .beginTransaction()
+//                        .replace(R.id.content_frame, new TabbedFragment(), TabbedFragment.TAG).commit();
                 break;
             case 1:
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.content_frame,
-                                new ConferenceListFragment(),
-                                ConferenceListFragment.TAG).commit();
+//                getSupportFragmentManager()
+//                        .beginTransaction()
+//                        .replace(R.id.content_frame,
+//                                new ConferenceListFragment(),
+//                                ConferenceListFragment.TAG).commit();
                 break;
         }
         mDrawerLayout.closeDrawer(mDrawerList);
@@ -149,5 +194,58 @@ public class NavigationDrawerActivity extends ActionBarActivity implements Artic
     public void setTitle(CharSequence title) {
         mTitle = title;
         getSupportActionBar().setTitle(mTitle);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in
+        // the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return new ArticleFragment();
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.title_section1).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_section2).toUpperCase(l);
+                case 2:
+                    return getString(R.string.title_section3).toUpperCase(l);
+            }
+            return null;
+        }
     }
 }
