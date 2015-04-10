dd710d67c08040f02f084961b1b928e28709763e
Timothy Asiimwe <atimothee@gmail.com>
2015-04-10 23:39:31 +0300

    added share action provider to article detail fragment

--- app/src/main/java/org/ieee/ieeehub/ArticleDetailActivity.java
+++ app/src/main/java/org/ieee/ieeehub/ArticleDetailActivity.java
@@ -1,15 +1,21 @@
 package org.ieee.ieeehub;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import org.ieee.ieeehub.fragment.ArticleDetailFragment;
 import org.ieee.ieeehub.fragment.ConferenceDetailFragment;
 
 
-public class ArticleDetailActivity extends ActionBarActivity {
+public class ArticleDetailActivity extends ActionBarActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,25 +31,7 @@ public class ArticleDetailActivity extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_article_detail, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/org/ieee/ieeehub/fragment/ArticleDetailFragment.java
+++ app/src/main/java/org/ieee/ieeehub/fragment/ArticleDetailFragment.java
@@ -1,12 +1,19 @@
 package org.ieee.ieeehub.fragment;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -22,8 +29,10 @@ import org.ieee.ieeehub.provider.article.ArticleSelection;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class ArticleDetailFragment extends Fragment implements LoaderManager.LoaderCallbacks{
+public class ArticleDetailFragment extends Fragment implements LoaderManager.LoaderCallbacks {
 
+    private static final String TAG = ArticleDetailFragment.class.getSimpleName();
+    private final String mShareString = "Check out this article on IEEE Spectrum";
     public static final String ARG_ITEM_ID = "item_id";
     private static final int ARTICLE_LOADER = 4;
     private Cursor mCursor;
@@ -35,8 +44,9 @@ public class ArticleDetailFragment extends Fragment implements LoaderManager.Loa
     public ArticleDetailFragment() {
     }
 
-    public static ArticleDetailFragment newInstance(Long id){
+    public static ArticleDetailFragment newInstance(Long id) {
         ArticleDetailFragment fragment = new ArticleDetailFragment();
+        fragment.setHasOptionsMenu(true);
         Bundle bundle = new Bundle();
         bundle.putLong(ARG_ITEM_ID, id);
         fragment.setArguments(bundle);
@@ -53,10 +63,10 @@ public class ArticleDetailFragment extends Fragment implements LoaderManager.Loa
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_article_detail, container, false);
-        titleTextView = (TextView)rootView.findViewById(R.id.article_detail_title);
-        textTextView = (TextView)rootView.findViewById(R.id.article_detail_text);
-        linkTextView = (TextView)rootView.findViewById(R.id.article_detail_link);
-        imageView = (ImageView)rootView.findViewById(R.id.article_detail_image);
+        titleTextView = (TextView) rootView.findViewById(R.id.article_detail_title);
+        textTextView = (TextView) rootView.findViewById(R.id.article_detail_text);
+        linkTextView = (TextView) rootView.findViewById(R.id.article_detail_link);
+        imageView = (ImageView) rootView.findViewById(R.id.article_detail_image);
         return rootView;
     }
 
@@ -69,17 +79,40 @@ public class ArticleDetailFragment extends Fragment implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader loader, Object data) {
-        mCursor = (Cursor)data;
+        mCursor = (Cursor) data;
         mCursor.moveToFirst();
         getActivity().setTitle(mCursor.getString(mCursor.getColumnIndex(ArticleColumns.TITLE)));
         titleTextView.setText(mCursor.getString(mCursor.getColumnIndex(ArticleColumns.TITLE)));
         textTextView.setText(mCursor.getString(mCursor.getColumnIndex(ArticleColumns.TEXT)));
         linkTextView.setText(mCursor.getString(mCursor.getColumnIndex(ArticleColumns.LINK)));
-        Picasso.with(getActivity()).load("http://spectrum.ieee.org"+mCursor.getString(mCursor.getColumnIndex(ArticleColumns.IMAGE))).into(imageView);
+        Picasso.with(getActivity()).load("http://spectrum.ieee.org" + mCursor.getString(mCursor.getColumnIndex(ArticleColumns.IMAGE))).into(imageView);
     }
 
     @Override
     public void onLoaderReset(Loader loader) {
 
+    }
+
+    private Intent createShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        shareIntent.setType("text/plain");
+        if(mCursor.getCount()!=0) {
+            mCursor.moveToFirst();
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mShareString + " - " + mCursor.getString(mCursor.getColumnIndex(ArticleColumns.TITLE)) + " " + mCursor.getString(mCursor.getColumnIndex(ArticleColumns.LINK)));
+        }
+            return shareIntent;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_article_detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareIntent());
+        } else {
+            Log.d(TAG, "Share action provider is null?");
+        }
     }
 }
